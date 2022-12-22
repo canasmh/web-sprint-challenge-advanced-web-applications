55449566f09e8302bbd71d44e93685b77ef240f3
canasmh <canasmh@yahoo.com>
2022-12-22 12:44:13 -0500

    Test the spinner

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,13 @@
 // Import the Spinner component into this file and test
+import React from "react"
+import { render } from "@testing-library/react"
+import Spinner from "./Spinner"
 // that it renders what it should for the different props it can take.
 test('sanity', () => {
-  expect(true).toBe(false)
+  const {getByText, queryByText, rerender} = render(<Spinner on={true} />)
+  const waitText = getByText(/Please wait.../i)
+  expect(waitText).toBeTruthy();
+  rerender(<Spinner on={false} />)
+  expect(queryByText(/Please wait.../i)).toBeNull
+  // expect(true).toBe(true)
 })
